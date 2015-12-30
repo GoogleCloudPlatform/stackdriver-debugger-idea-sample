73c24bfd623a61987de093a573b898a563550e2e
Elliotte Rusty Harold <elharo@macfaq.com>
2015-12-30 16:13:51 -0500

    add tests

--- src/test/java/com/google/gct/tutorial/HelloBrowserServletTest.java
+++ src/test/java/com/google/gct/tutorial/HelloBrowserServletTest.java
@@ -18,4 +18,22 @@ public class HelloBrowserServletTest {
     Assert.assertTrue(response.getBody().contains("Hello Chrome"));
   }
 
+  @Test
+  public void testSafari() throws IOException {
+    StubHttpServletRequest request = new StubHttpServletRequest();
+    request.setHeader("UserAgent", "Safari");
+    SpyHttpServletResponse response = new SpyHttpServletResponse();
+    servlet.doGet(request, response);
+    Assert.assertTrue(response.getBody().contains("Hello Safari"));
+  }
+
+  @Test
+  public void testFirefox() throws IOException {
+    StubHttpServletRequest request = new StubHttpServletRequest();
+    request.setHeader("UserAgent", "Firefox");
+    SpyHttpServletResponse response = new SpyHttpServletResponse();
+    servlet.doGet(request, response);
+    Assert.assertTrue(response.getBody().contains("Hello Firefox"));
+  }
+
 }
