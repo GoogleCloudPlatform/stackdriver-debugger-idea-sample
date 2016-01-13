e94583ba3c6d84d1c92267d4752dcd7c06a9437b
eshaul <eshaul@google.com>
2016-01-13 17:26:35 -0500

    test cm

--- src/main/java/com/google/gct/tutorial/HelloBrowserServlet.java
+++ src/main/java/com/google/gct/tutorial/HelloBrowserServlet.java
@@ -43,7 +43,8 @@ public class HelloBrowserServlet extends HttpServlet {
         displayName = userAgent; 
       }
     }
-    
+
+    //test
     response.setContentType("text/html");
     PrintWriter writer = response.getWriter();
 
